0c2a83a4a8f1a27bcc036d308497441ba00c6c3c
PapaShams <shamwamadennis@gmail.com>
2022-03-15 08:37:11 -0700

    Added my first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* prints _putchar, followed by a new line.
+*
+* Return: void.
+*/
+
+int main(void)
+{
+int i;
+char ch[] = "_putchar";
+
+for (i = 0;i < 8; i++)
+{
+_putchar(ch[]);
+}
+_putchar("\n");
+return (0);
+}
