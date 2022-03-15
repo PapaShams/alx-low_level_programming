f936c90b06c383c8830efdafed66ff26bdef9331
PapaShams <shamwamadennis@gmail.com>
2022-03-15 15:08:39 -0700

    added task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,16 @@ void print_alphabet_x10(void)
 {
 int i;
 char c;
-i=0;
+i = 0;
 while (i < 10)
 {
 c = 'a';
-while(c <= 'z')
+while (c <= 'z')
 {
 _putchar(c);
 c++;
 }
 _putchar('\n');
-i++;     
+i++;   
 }
 }
